e7e65418dee6835a8fb4978fb96cd24f8a76f90c
TeRed <dddd.albert@gmail.com>
2017-06-12 16:16:44 +0200

    git

--- Kod3.c
+++ Kod3.c
@@ -8,19 +8,18 @@ void inorder(struct node *root)
         inorder(root->right);
     }
 }
-  
-/* A utility function to insert a new node with given key in BST */
+//stosowny komenatrz
 struct node* insert(struct node* node, int key)
 {
     /* If the tree is empty, return a new node */
     if (node == NULL) return newNode(key);
- 
+
     /* Otherwise, recur down the tree */
     if (key < node->key)
         node->left  = insert(node->left, key);
     else if (key > node->key)
-        node->right = insert(node->right, key);   
- 
+        node->right = insert(node->right, key);
+
     /* return the (unchanged) node pointer */
     return node;
 }
