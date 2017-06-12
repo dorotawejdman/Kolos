8e8f4fab557b438acb0213852f9935a9236774d4
TeRed <dddd.albert@gmail.com>
2017-06-12 16:05:44 +0200

    git push

---
+++ Kod2.c
@@ -0,0 +1,23 @@
+// Driver Program to test above functions
+int main()
+{
+    /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+    struct node *root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    // print inoder traversal of the BST
+    inorder(root);
+
+    return 0;
+}
